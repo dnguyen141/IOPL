994eedda7064fd4bf484f5eabf00868b16a230c1
Dinh Nguyen <minhdinhnguyenpp1495@gmail.com>
2024-02-15 16:47:41 +0100

    change user repository for pagination

--- LibraryManager/src/main/java/inst/iop/LibraryManager/authentication/repositories/UserRepository.java
+++ LibraryManager/src/main/java/inst/iop/LibraryManager/authentication/repositories/UserRepository.java
@@ -2,13 +2,14 @@ package inst.iop.LibraryManager.authentication.repositories;
 
 import inst.iop.LibraryManager.authentication.entities.User;
 import inst.iop.LibraryManager.authentication.entities.enums.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -21,14 +22,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
   @Query("SELECT u FROM User u WHERE u.id = :id")
   Optional<User> findUserById(Long id);
 
-  @Query("SELECT u FROM User u WHERE u.role = 'USER'")
-  List<User> findAllUsers();
+  @Query("SELECT u FROM User u WHERE u.role = 'USER' ORDER BY u.id")
+  Page<User> findAllUsers(Pageable pageable);
 
-  @Query("SELECT u FROM User u WHERE u.role = 'MODERATOR'")
-  List<User> findAllModerators();
-
-  @Query("SELECT u FROM User u WHERE u.role = 'ADMIN'")
-  List<User> findAllAdmins();
+  @Query("SELECT u FROM User u WHERE u.role = 'MODERATOR' or u.role = 'USER' ORDER BY u.id")
+  Page<User> findAllModeratorsAndUsers(Pageable pageable);
 
   @Modifying
   @Query("UPDATE User u " +
@@ -46,7 +44,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
       "u.lastName = CASE WHEN (:lastName is not null) THEN :lastName ELSE u.lastName END, " +
       "u.role = CASE WHEN (:role is not null) THEN :role ELSE u.role END " +
       "WHERE u.id = :id")
-  void updateUserByEmail(Long id, String password, String firstName, String lastName, Role role);
+  void updateUserById(Long id, String password, String firstName, String lastName, Role role);
 
   @Modifying
   @Query("DELETE FROM User u WHERE u.id = :id")
