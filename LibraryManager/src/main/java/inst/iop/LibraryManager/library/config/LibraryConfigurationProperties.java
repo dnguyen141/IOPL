c2ea326217d08b2b0a994c8f3a358b0595b1326a
Dinh Nguyen <minhdinhnguyenpp1495@gmail.com>
2024-02-15 16:59:15 +0100

    add default folder name for cover images upload

--- LibraryManager/src/main/java/inst/iop/LibraryManager/library/config/LibraryConfigurationProperties.java
+++ LibraryManager/src/main/java/inst/iop/LibraryManager/library/config/LibraryConfigurationProperties.java
@@ -1,4 +1,4 @@
-package inst.iop.LibraryManager.library.configs;
+package inst.iop.LibraryManager.library.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
