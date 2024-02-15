35edd556689b13453a7057ce1223a7171988c57f
Dinh Nguyen <minhdinhnguyenpp1495@gmail.com>
2024-02-15 17:07:25 +0100

    add rest template bean

--- LibraryManager/src/main/java/inst/iop/LibraryManager/library/config/LibraryConfig.java
+++ LibraryManager/src/main/java/inst/iop/LibraryManager/library/config/LibraryConfig.java
@@ -1,8 +1,16 @@
-package inst.iop.LibraryManager.library.configs;
+package inst.iop.LibraryManager.library.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
 
 @Configuration
 public class LibraryConfig {
 
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
 }
