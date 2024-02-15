c14f8d2d806c2ad7c817fec731cf92803f305ffe
Dinh Nguyen <minhdinhnguyenpp1495@gmail.com>
2024-02-15 16:54:40 +0100

    add more handler for exceptions

--- LibraryManager/src/main/java/inst/iop/LibraryManager/utilities/advices/GlobalRestControllerAdvice.java
+++ LibraryManager/src/main/java/inst/iop/LibraryManager/utilities/advices/GlobalRestControllerAdvice.java
@@ -2,7 +2,11 @@ package inst.iop.LibraryManager.utilities.advices;
 
 import inst.iop.LibraryManager.utilities.exceptions.BadRequestDetailsException;
 import inst.iop.LibraryManager.utilities.responses.ApiResponseEntityFactory;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Path;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
@@ -13,21 +17,28 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.security.SignatureException;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static inst.iop.LibraryManager.utilities.ConstraintViolationSetHandler.convertConstrainViolationSetToMap;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @RequiredArgsConstructor
 @RestControllerAdvice
 public class GlobalRestControllerAdvice extends ResponseEntityExceptionHandler {
+
+  @Value("${spring.servlet.multipart.max-file-size}")
+  private String maxUploadSize;
 
   private final ApiResponseEntityFactory responseEntityFactory;
 
@@ -78,9 +89,39 @@ public class GlobalRestControllerAdvice extends ResponseEntityExceptionHandler {
     return responseEntityFactory.createErrorResponse(HttpStatus.METHOD_NOT_ALLOWED, ex.getMessage());
   }
 
-  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-  public ResponseEntity<Object> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
-    return responseEntityFactory.createErrorResponse(HttpStatus.FORBIDDEN,
-        "Provided type for requested argument(s) is in wrong type");
+  @Override
+  protected ResponseEntity<Object> handleMaxUploadSizeExceededException(
+      MaxUploadSizeExceededException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+    return responseEntityFactory.createErrorResponse(HttpStatus.PAYLOAD_TOO_LARGE,
+        "Maximum upload size is " + maxUploadSize
+    );
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMissingServletRequestParameter(
+      MissingServletRequestParameterException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+    return responseEntityFactory.createErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
+  }
+
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException e) {
+    Map<String, String> violations = new HashMap<>();
+
+    for (ConstraintViolation<?> violation : e.getConstraintViolations()) {
+      String field = extractFieldNameFromViolation(violation);
+      String message = violation.getMessage();
+      violations.put(field, message);
+    }
+
+    return responseEntityFactory.createErrorWithDetailsResponse(HttpStatus.BAD_REQUEST,
+        "Constraint(s) violation detected", violations);
+  }
+
+  private static String extractFieldNameFromViolation(ConstraintViolation<?> violation) {
+    String fieldName = null;
+    for (Path.Node node : violation.getPropertyPath()) {
+      fieldName = node.getName();
+    }
+    return fieldName;
   }
 }
