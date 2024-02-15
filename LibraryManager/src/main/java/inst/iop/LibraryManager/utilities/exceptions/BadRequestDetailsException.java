8754f0d5e44bfdb39b5e908e99db0c376f45c7a9
Dinh Nguyen <minhdinhnguyenpp1495@gmail.com>
2024-02-15 16:54:13 +0100

    modify exception to add message

--- LibraryManager/src/main/java/inst/iop/LibraryManager/utilities/exceptions/BadRequestDetailsException.java
+++ LibraryManager/src/main/java/inst/iop/LibraryManager/utilities/exceptions/BadRequestDetailsException.java
@@ -10,5 +10,22 @@ import java.util.Map;
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 public class BadRequestDetailsException extends RuntimeException {
-  private final Map<String, Object> violations;
+  private final String message;
+  private final Map<String, String> violations;
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder("Error: " + message + " - ");
+    int index = 0;
+    for (Map.Entry<String, String> entry : violations.entrySet()) {
+      sb.append(entry.getKey()).append(": ").append(entry.getValue());
+      if (index < violations.size() - 1) {
+        sb.append("; ");
+        index++;
+      } else {
+        sb.append(".");
+      }
+    }
+    return sb.toString();
+  }
 }
